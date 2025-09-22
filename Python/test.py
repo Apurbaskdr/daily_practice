4425c9fb6291c69301a1f406d8ebca86c9be8b99
Apurba Sikdar <your_github_email@example.com>
2025-09-22 08:26:39 +0530

    changes

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,30 @@
-key='Apurba'
-key_b=key.encode()
-print (key_b)
-hash_code=sum(key_b)
-print (hash_code)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+    def build_linked_list(arr):
+        head=None
+        tail=None
+        for num in arr:
+            node=ListNode(num)
+            if not head:
+                head=node
+                tail=node
+            else:
+                tail.next=node
+                tail=node
+
+        return head
+    
+    
+
+class Solution:
+    def rotateRight(self, head: list, k: int):
+        print(head,k)
+
+
+
+head=[1,2,3,4,5]
+k=5
+print (head,k)
